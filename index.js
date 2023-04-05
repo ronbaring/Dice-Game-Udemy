3746e2cf62d4a7b57945ee4662a0a0967a4c3716
Ronoel Baring <86248397+ronbaring@users.noreply.github.com>
2023-04-05 16:53:56 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -8,24 +8,29 @@ diceResults.push("images/dice6.png");
 
 
 function rollDice() {
-    
-var randomnumber1 = Math.floor(Math.random() * 6);
-var randomnumber2 = Math.floor(Math.random() * 6);
+
+    var randomnumber1 = Math.floor(Math.random() * 6);
+    var randomnumber2 = Math.floor(Math.random() * 6);
 
     document.querySelectorAll("img")[0].setAttribute("src", diceResults[randomnumber1]);
     document.querySelectorAll("img")[1].setAttribute("src", diceResults[randomnumber2]);
 
     if (randomnumber1 > randomnumber2) {
 
-        document.querySelector("h1").innerHTML = " 🚩 Player 1 Wins!";
+        document.querySelector("h1").innerHTML = " Player 1 Wins!";
+        document.querySelectorAll("p")[0].textContent = "Player 1 🚩";
+        document.querySelectorAll("p")[1].textContent = "Player 2";
 
     } else if (randomnumber2 > randomnumber1) {
 
-        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩 ";
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+        document.querySelectorAll("p")[1].textContent = "Player 2 🚩";
+        document.querySelectorAll("p")[0].textContent = "Player 1";
 
     } else if (randomnumber1 = randomnumber2) {
 
         document.querySelector("h1").innerHTML = "Draw!";
-
+        document.querySelectorAll("p")[0].textContent = "Player 1";
+        document.querySelectorAll("p")[1].textContent = "Player 2";
     }
 }
